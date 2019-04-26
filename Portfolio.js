30eecae32998bdf4ade307bb16644d9ec70afda6
Nia Reddix <nreddix24@gmail.com>
2019-04-26 18:55:58 -0400

    edit portfolio page

--- Portfolio.js
+++ Portfolio.js
@@ -1,4 +1,21 @@
+function newTab() {
+    var form = document.createElement("form");
+    form.method = "GET";
+    form.action = "https://www.linkedin.com/in/nia-reddix-22ba61126/";
+    form.target = "_blank";
+    document.body.appendChild(form);
+    form.submit();
+}
 
 
-window.open("https://www.linkedin.com/in/nia-reddix-22ba61126/");
-window.open("https://github.com/nreddix");
+
+// var linkedin = "https://www.linkedin.com/in/nia-reddix-22ba61126/";
+// window.open("https://www.linkedin.com/in/nia-reddix-22ba61126/", "_blank");
+ 
+
+
+// window.open("https://github.com/nreddix", "blank")
+  
+
+// var url = "http://www.example.com";
+//  window.open(url, '_blank');
